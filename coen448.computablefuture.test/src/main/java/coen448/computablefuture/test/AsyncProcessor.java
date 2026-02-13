841a7952f2c154f5bba9044cf622b84dad4b5e6c
p-nikyem <92038777+p-nikyem@users.noreply.github.com>
2026-02-13 02:14:46 -0500

    added code for task A

--- coen448.computablefuture.test/src/main/java/coen448/computablefuture/test/AsyncProcessor.java
+++ coen448.computablefuture.test/src/main/java/coen448/computablefuture/test/AsyncProcessor.java
@@ -5,9 +5,33 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class AsyncProcessor {
 	
+    public CompletableFuture<String> processAsyncFailFast(
+            List<Microservice> services,
+            List<String> messages) {
+
+        if (services == null || messages == null) {
+            return CompletableFuture.failedFuture(
+                new IllegalArgumentException("services and messages must not be null"));
+        }
+        if (services.size() != messages.size()) {
+            return CompletableFuture.failedFuture(
+                new IllegalArgumentException("services and messages must have the same size"));
+        }
+
+        List<CompletableFuture<String>> futures = IntStream.range(0, services.size())
+            .mapToObj(i -> services.get(i).retrieveAsync(messages.get(i)))
+            .collect(Collectors.toList());
+
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+            .thenApply(v -> futures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.joining(" ")));
+    }
+
     public CompletableFuture<String> processAsync(List<Microservice> microservices, String message) {
     	
         List<CompletableFuture<String>> futures = microservices.stream()
